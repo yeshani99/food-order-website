7db7924146945d77c8bab6a93b8a053889b4d6a4
yeshani Eranaga <ehettithantri@gmail.com>
2025-04-07 23:03:08 +0530

    change about

--- src/components/About.js
+++ src/components/About.js
@@ -18,12 +18,10 @@ const About = () => {
           Food Is An Important Part Of A Balanced Diet
         </h1>
         <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        Welcome! We’re passionate about creating meaningful digital experiences that blend functionality with thoughtful design. Our journey is driven by curiosity, creativity, and a commitment to continuous improvement. Whether it's building solutions, solving problems, or simply exploring new ideas, we believe in adding value every step of the way.
         </p>
         <p className="primary-text">
-          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
-          facilisis at fringilla quam.
+        Little attention is given to form or refinement. Sudden tension brings discomfort, softened only by strong support at the boundaries.
         </p>
         <div className="about-buttons-container">
           <button className="secondary-button">Learn More</button>
